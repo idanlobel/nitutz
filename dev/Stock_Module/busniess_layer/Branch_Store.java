97fecd87a45191e9a22106e9c1223f4d0ebc0745
DenisPn <panor@post.bgu.co.il>
2022-05-19 21:39:07 +0300

    ignore

--- dev/Stock_Module/busniess_layer/Branch_Store.java
+++
@@ -1,9 +0,0 @@
-package Stock_Module.busniess_layer;
-public class Branch_Store {
-
-    private String location;
-    private Stock stock;
-    private int ID;
-
-
-}
--- dev/Stock_Module/busniess_layer/Stores.java
+++
@@ -1,9 +0,0 @@
-package Stock_Module.busniess_layer;
-
-import java.util.List;
-
-public class Stores {
-
-    List<Branch_Store> branch_stores;
-
-}
