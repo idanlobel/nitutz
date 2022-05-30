6ff0e5c2e3e02437fc7b9b09452fa6a8700f54a3
Denis <panor@post.bgu.ac.il>
2022-05-30 10:56:53 +0300

    implemented threaded handling of shortage and periodic orders, for now hard coded to be every day at 5 am

--- dev/Stock_Module/stock_presntation_layer/Merged_main.java
+++ dev/Stock_Module/stock_presntation_layer/Merged_main.java
@@ -4,8 +4,17 @@ import Stock_Module.stock_service_layer.Stock_Manager;
 import SuppliersModule.SuppliersPresentationLayer.SuppliersMain;
 import SuppliersModule.SuppliersServiceLayer.SupplyModuleService;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalUnit;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Merged_main {
     public static void main(String[] args) throws Exception {
@@ -16,6 +25,11 @@ public class Merged_main {
         String current_order="";
         String[] order_in_array=new String[200];
         SuppliersMain suppliersMain=new SuppliersMain();
+        ScheduledExecutorService executorService=Executors.newScheduledThreadPool(2);
+        executorService.scheduleAtFixedRate(() -> System.out.println(stock_manager.check_shortage_order().getValue()),
+                LocalDateTime.now().until(LocalDateTime.of(LocalDate.now().plusDays(1),LocalTime.of(5,0)), ChronoUnit.MINUTES),60*24, TimeUnit.MINUTES);
+        executorService.scheduleAtFixedRate(() -> System.out.println(stock_manager.check_periodic_order().getValue()),
+                LocalDateTime.now().until(LocalDateTime.of(LocalDate.now().plusDays(1),LocalTime.of(5,0)), ChronoUnit.MINUTES),60*24, TimeUnit.MINUTES);
         System.out.println("if you want to load stock data please type yes");
         current_order=scanner.nextLine();
         if(current_order.equals("yes"))
@@ -26,10 +40,8 @@ public class Merged_main {
 
         while(!current_order.equals("exit"))
         {
-            System.out.println(stock_manager.check_shortage_order().getValue());
-            System.out.println(stock_manager.check_periodic_order().getValue());
-
-            System.out.println("please type stock or supplier to access the desired functions");
+            System.out.println("please type stock or supplier to access the desired functions.\n" +
+                    "to handle scheduled actions now, type handle");
             current_order=scanner.nextLine();
             order_in_array=current_order.split(" ");
             if(order_in_array[0].toLowerCase(Locale.ROOT).equals("stock"))
@@ -41,17 +53,18 @@ public class Merged_main {
 
 
             }
-            else
-            {
-                if(order_in_array[0].toLowerCase(Locale.ROOT).equals("supplier"))
+            else if(order_in_array[0].toLowerCase(Locale.ROOT).equals("supplier"))
                 {
                     suppliersMain.RunSuppliers(serviceObj,stock_manager);
                 }
-                else
+            else if(order_in_array[0].toLowerCase(Locale.ROOT).equals("handle")){
+                System.out.println(stock_manager.check_shortage_order().getValue());
+                System.out.println(stock_manager.check_periodic_order().getValue());
+            }
+            else
                 {
                     System.out.println("please enter stock or supplier.\n");
                 }
-            }
 
         }
         System.out.println("exiting.. see you later");
